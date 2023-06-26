629943ea97a40b2a85a820b0c73d446d8cfcccbe
M Usman <igz.iwd.29@gmail.com>
2023-06-26 15:28:07 +0500

    changes in

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,14 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import UseSignup from "../customHooks/UseSignup";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import Navbar from "../components/navbar";
 
 import "../App.css"
 
 function Signup() {
-  const { getUserData, addDataButton } = UseSignup();
+  // const { getUserData } = UseSignup();
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({
+    
+  });
+
+  // name: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+
+   const [userData, setUserData] = useState([]);
+
+  const [signupInputs, setSignupInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+
+    setSignupInputs(() => {
+      return {
+        ...signupInputs,
+        [name]: value,
+      };
+    });
+  };
+
+
+
+   const addDataButton = (e) => {
+    e.preventDefault();
+    const { name, email, password, confirmPassword } = signupInputs;
+
+     if (name === "") {
+      setErrors({name: 'Name field is required'})
+      // alert("name field is required");
+     }
+    //  else if (email === "") {
+    //   alert("email field is required");
+    // } else if (password === "") {
+    //   alert("password field is required");
+    // } else if (confirmPassword === "") {
+    //   alert("confirm Password field is required"); //Confrirm password must match with password
+    // } else if (password !== confirmPassword) {
+    //   alert("Password and confirm Password should be same"); // Password and confirm password must match
+    // } else if (!email.includes("@")) {
+    //   alert(" Enter valid Email");
+    // } else if (password.length < 5) {
+    //   alert("Enter Password strong or greater than 5 digits "); //Your password length must be greater than 5
+    //  }
+     console.log(errors.name);
+     if (!errors) {
+        navigate("/login")
+        alert("Enter data successfully");
+       }else {
+      // alert("Enter data successfully"); // The account has been created successfully.
+      // localStorage.setItem("key", JSON.stringify([...userData, signupInputs]));
+      // dompush("/Login");
+    }
+  };
 
   return (
    <div >
@@ -17,7 +79,7 @@ function Signup() {
       <Row className="justify-content-center" >
         <Col md={6} className="mt-4">
           <h2 className="text-center mb-4">Create an Account</h2>
-          <Form>
+          <Form method="post" onSubmit={addDataButton}>
             <Form.Group controlId="formBasicName">
               <Form.Label>Name</Form.Label>
               <Form.Control
@@ -25,8 +87,11 @@ function Signup() {
                 placeholder="Enter your name"
                 name="name"
                 required
-                onChange={ getUserData}
-              />
+                onChange={ handleChange}
+                />
+                <Form.Text className="text-danger">
+                { errors.name !== undefined && errors.name && errors.name}
+                </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="formBasicEmail">
@@ -36,7 +101,7 @@ function Signup() {
                 placeholder="Enter email"
                 name="email"
                 required
-                onChange={ getUserData}
+                onChange={ handleChange}
               />
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
@@ -50,7 +115,7 @@ function Signup() {
                 placeholder="Password"
                 name="password"
                 required
-                onChange={ getUserData}
+                onChange={ handleChange}
               />
             </Form.Group>
 
@@ -59,20 +124,20 @@ function Signup() {
               <Form.Control
                 type="password"
                 placeholder="Confirm Password"
-                onChange={ getUserData}
+                onChange={ handleChange}
                 name="confirmPassword"
 
                 required
               />
-            </Form.Group>
-
-            <Button variant="info" type="submit" className="w-100 mt-3" onClick={addDataButton}>
+              </Form.Group>
+              
+              {/* onClick={addDataButton} */}
+            <Button variant="info" type="submit" className="w-100 mt-3" >
               Sign Up
             </Button>
 
           </Form>
           <p className="mt-4">Already have a account  <span><NavLink to="/Login">Login</NavLink></span></p>
-         
         </Col>
       </Row>
     </Container>
--- src/customHooks/UseSignup.js
+++ src/customHooks/UseSignup.js
@@ -1,6 +1,5 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 
 function UseSignup() {
   const [signupInputs, setSignupInputs] = useState({
@@ -9,9 +8,8 @@ function UseSignup() {
     password: "",
     confirmPassword: "",
   });
-  const dompush=useNavigate()
-  const[userData,setUserData]=useState([])
-
+  const dompush = useNavigate();
+  const [userData, setUserData] = useState([]);
 
   const getUserData = (e) => {
     const { value, name } = e.target;
@@ -20,38 +18,34 @@ function UseSignup() {
       return {
         ...signupInputs,
         [name]: value,
-     
       };
     });
   };
 
-  const addDataButton = (e) => {
-    e.preventDefault()
-    const { name, email, password, confirmPassword } = signupInputs;
-   
+  // const addDataButton = (e) => {
+  //   e.preventDefault();
+  //   const { name, email, password, confirmPassword } = signupInputs;
 
-    if (name === "") {
-      alert("name field is required");
-    } else if (email === "") {
-      alert("email field is required");
-    } else if (password === "") {
-      alert("password field is required");
-    } else if (confirmPassword === "") {
-      alert("confirm Password field is required");
-    } else if (password !== confirmPassword) {
-      alert("Password and confirm Password should be same");
-    } else if (!email.includes("@")) {
-      alert(" Enter valid Email");
-    } else if (password.length < 5) {
-      alert("Enter Password strong or greater than 5 digits ");
-    } else {
-      alert("Enter data successfully");
-      localStorage.setItem("key",JSON.stringify([...userData,signupInputs]))
-      dompush("/Login")
-     
-
-    }
-  };
+  //   if (name === "") {
+  //     alert("name field is required");
+  //   } else if (email === "") {
+  //     alert("email field is required");
+  //   } else if (password === "") {
+  //     alert("password field is required");
+  //   } else if (confirmPassword === "") {
+  //     alert("confirm Password field is required"); //Confrirm password must match with password
+  //   } else if (password !== confirmPassword) {
+  //     alert("Password and confirm Password should be same"); // Password and confirm password must match
+  //   } else if (!email.includes("@")) {
+  //     alert(" Enter valid Email");
+  //   } else if (password.length < 5) {
+  //     alert("Enter Password strong or greater than 5 digits "); //Your password length must be greater than 5
+  //   } else {
+  //     alert("Enter data successfully"); // The account has been created successfully.
+  //     localStorage.setItem("key", JSON.stringify([...userData, signupInputs]));
+  //     dompush("/Login");
+  //   }
+  // };
 
   return {
     getUserData,
