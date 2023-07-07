0428dfa5d99a71c695061146cae575699e77b151
M Usman <igz.iwd.29@gmail.com>
2023-07-07 12:43:24 +0500

    test

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -18,9 +18,56 @@ function HomePage() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const [pageCount, setPageCount] = useState(0);
+  const [loader, setLoader] = useState(true);
 
   const dispatch = useDispatch();
 
+  /* useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch(
+        "https://dummyjson.com/products/?limit=10&skip=10&select=title,price,thumbnail"
+      );
+      const data = await response.json();
+
+      setPageCount(Math.ceil(data.total / itemsPerPage));
+      // setProductsData(data.products);
+      // setLoader(false)
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }; */
+  
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const initialProducts = await fetchComments(currentPage);
+      setProductsData(initialProducts);
+    };
+    fetchData();
+  }, [currentPage]);
+
+  const fetchComments = async (currentPage) => {
+    try {
+      const skip = (currentPage - 1) * itemsPerPage;
+      const response = await fetch(
+        `https://dummyjson.com/products/?limit=10&skip=${skip}&select=title,price,thumbnail`
+        // `https://dummyjson.com/products/?_page=${currentPage}&limit=10&skip=${skip}&select=title,price,thumbnail`
+      );
+      const data = await response.json();
+      setPageCount(Math.ceil(data.total / itemsPerPage));
+      return data.products;
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      return [];
+    }
+  };
+
+  
   const addCartItem = (id) => {
     const speech = new SpeechSynthesisUtterance("Item added into cart successfully");
 
@@ -37,51 +84,10 @@ function HomePage() {
   const opencart = () => {
     dispatch(addvalue(true));
   };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const getProducts = async () => {
-    try {
-      const response = await fetch(
-        "https://dummyjson.com/products/?limit=10&skip=10&select=title,price,thumbnail"
-      );
-      const data = await response.json();
-      setPageCount(Math.ceil(data.total / itemsPerPage));
-      setProductsData(data.products);
-    } catch (error) {
-      console.log("Error:", error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const initialProducts = await fetchComments(currentPage);
-      setProductsData(initialProducts);
-    };
-    fetchData();
-  }, [currentPage]);
-
-  const fetchComments = async (currentPage) => {
-    try {
-      const skip = (currentPage - 1) * itemsPerPage;
-      const response = await fetch(
-        `https://dummyjson.com/products/?_page=${currentPage}&limit=10&skip=${skip}&select=title,price,thumbnail`
-      );
-      const data = await response.json();
-      return data.products;
-    } catch (error) {
-      console.log("Error fetching data:", error);
-      return [];
-    }
-  };
-
   const handlePageClick = async (data) => {
     const selectedPage = data.selected + 1;
     setCurrentPage(selectedPage);
   };
-
   return (
     <div className="container-fluid">
       <Navbar color="bg-info" />
@@ -114,9 +120,14 @@ function HomePage() {
         </div>
       </div>
 
+      {/* {loader && <div className="col-12 d-flex justify-content-center mt-5">
+            <Audio type="Oval" color="#00BFFF" height={100} width={100} />
+          </div>} */}
+
       <div className="row mt-3">
         {productData.length === 0 ? (
           <div className="col-12 d-flex justify-content-center mt-5">
+            OK
             <Audio type="Oval" color="#00BFFF" height={100} width={100} />
           </div>
         ) : (
