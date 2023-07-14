44649c7983e626ffc8c6e9f53cdc22410a799b63
-e <asmarnaeem35@gmail.com>
2023-07-14 11:32:01 +0500

    updated

--- src/Pages/Productdetail.jsx
+++ src/Pages/Productdetail.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useParams, usenavigate, useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
-import { useParams } from "react-router-dom";
 import Footer from "../components/footer";
-import "../App.css";
 import { addToCart } from "../Store/AddCartSlice";
 import { useDispatch } from "react-redux";
 import { addvalue } from "../Store/CartSlice";
@@ -11,11 +10,11 @@ import { Audio } from "react-loader-spinner";
 const Productdetail = () => {
   const { paramid } = useParams();
   const [productData, setProductsData] = useState([]);
+  // const navigate = useNavigate();
 
   useEffect(() => {
     getProducts();
   }, []);
-  console.log("products Data is :",productData);
 
   const getProducts = async () => {
     try {
@@ -27,94 +26,99 @@ const Productdetail = () => {
     }
   };
 
- 
-
   const dispatch = useDispatch();
   const addCartItem = (id) => {
-
     dispatch(addToCart(id));
     dispatch(addvalue(true));
+  };
+
+  const renderProductImage = () => {
+    if (productData.length === 0) {
+      return (
+        <div className="col-12 d-flex justify-content-center mt-5">
+          <Audio type="Oval" color="#00BFFF" height={100} width={100} />
+        </div>
+      );
+    }
+
+    const selectedProduct = productData.find(
+      (product) => product.id === parseInt(paramid)
+    );
+
+    if (!selectedProduct) {
+      return <h3>Page Not Found</h3>;
+    }
+
+    return (
+      <div key={selectedProduct.id}>
+        <img
+          className="card-img-top"
+          src={selectedProduct.images[0]}
+          alt={selectedProduct.title}
+        />
+      </div>
+    );
+  };
+
+  const renderProductDetails = () => {
+    if (productData.length === 0) {
+      return (
+        <div className="col-12 d-flex justify-content-center mt-5">
+          <Audio type="Oval" color="#00BFFF" height={100} width={100} />
+        </div>
+      );
+    }
+
+    const selectedProduct = productData.find(
+      (product) => product.id === parseInt(paramid)
+    );
+
+    if (!selectedProduct) {
+      return <h3>Page Not Found</h3>;
+    }
+
+    return (
+      <div className="card-body">
+        <h2 className="card-title">{selectedProduct.title}</h2>
+        <p className="card-text">{selectedProduct.description}</p>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            Price: ${selectedProduct.price}
+          </li>
+          <li className="list-group-item">
+            Discount: {selectedProduct.discountPercentage}%
+          </li>
+          <li className="list-group-item">
+            Rating: {selectedProduct.rating}/5
+          </li>
+          <li className="list-group-item">Brand: {selectedProduct.brand}</li>
+          <li className="list-group-item">
+            Stock: {selectedProduct.stock} units
+          </li>
+          <button
+            className="mybtn btn bg-info text-white"
+            type="button"
+            data-toggle="button"
+            aria-pressed="false"
+            autoComplete="off"
+            onClick={() => addCartItem(selectedProduct.id)}
+          >
+            Add to Cart
+          </button>
+        </ul>
+      </div>
+    );
   };
 
   return (
     <div>
       <Navbar />
-      
       <div className="row justify-content-evenly mt-4 mt-5 mb-5">
         <div className="col-md-5">
-          <div className="card">
-            {productData.length === 0 ? (
-              <div className="col-12 d-flex justify-content-center mt-5">
-                <Audio type="Oval" color="#00BFFF" height={100} width={100} />
-              </div>
-            ) : (
-              productData.map((product) => {
-                if (product.id == parseInt(paramid)) {
-                  console.log("test", product.id == paramid);
-                  return (
-                    <div key={product.id}>
-                      <img
-                        className="card-img-top"
-                        src={product.images[0]}
-                        alt={product.title}
-                      />
-                    </div>
-                  );
-                }
-                return null;
-              })
-            )}
-          </div>
+          <div className="card">{renderProductImage()}</div>
         </div>
         <div className="col-md-5">
-          <div className="card">
-            {productData.length === 0 ? (
-              <div className="col-12 d-flex justify-content-center mt-5">
-                <Audio type="Oval" color="#00BFFF" height={100} width={100} />
-              </div>
-            ) : (
-              productData.map((product) => {
-                if (product.id == parseInt(paramid)) { 
-                  return (
-                    <div key={product.id}>
-                      <div className="card-body">
-                        <h2 className="card-title">{product.title}</h2>
-                        <p className="card-text">{product.description}</p>
-                        <ul className="list-group list-group-flush">
-                          <li className="list-group-item">
-                            Price: ${product.price}
-                          </li>
-                          <li className="list-group-item">
-                            Discount: {product.discountPercentage}%
-                          </li>
-                          <li className="list-group-item">
-                            Rating: {product.rating}/5
-                          </li>
-                          <li className="list-group-item">
-                            Brand: {product.brand}
-                          </li>
-                          <li className="list-group-item">
-                            Stock: {product.stock} units
-                          </li>
-                          <button
-                            className="mybtn btn bg-info text-white"
-                            type="button"
-                            data-toggle="button"
-                            aria-pressed="false"
-                            autoComplete="off"
-                            onClick={() => addCartItem(product.id)}
-                          >
-                            Add to Cart
-                          </button>
-                        </ul>
-                      </div>
-                    </div>
-                  );
-                }
-                return null;
-              })
-            )}
-          </div>
+          <div className="card">{renderProductDetails()}</div>
         </div>
       </div>
       <Footer />
